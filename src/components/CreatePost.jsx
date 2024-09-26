e8590e457895e0f741b949ef712a1353790721dc
vinod kumar <88377243+codervinod123@users.noreply.github.com>
2024-09-26 21:58:13 +0530

    feat:trending posts list added

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -7,42 +7,44 @@ import { Spin } from "antd";
 import axios from "axios";
 
 const CreatePost = () => {
-
-  const [image,setImage]=useState({
-     preview:null,
-     dbImage:null
-  })
-  const [content,setContent]=useState("");
-  const [loading, setLoading]=useState(false);
-  const handleInputChange=(e)=>{
+  const [image, setImage] = useState({
+    preview: null,
+    dbImage: null,
+  });
+  const [content, setContent] = useState("");
+  const [loading, setLoading] = useState(false);
+  const handleInputChange = (e) => {
     setContent(e.target.value);
-  }
-  const handleImageChange=(e)=>{
+  };
+  const handleImageChange = (e) => {
     const img = {
       preview: URL.createObjectURL(e.target.files[0]),
       dbImage: e.target.files[0],
     };
     setImage(img);
-  }
-  const handleSubmitForm=async()=>{
-     const formData=new FormData();
-     formData.append("content",content);
-     formData.append("file",image.dbImage);
-     setLoading(true);
-     const response=await axios.post("http://localhost:3001/api/v1/tweet", formData);
-     if(response){
-         setLoading(false);
-     }
-     console.log(response);
-     setImage({ preview: "", data: "" });
-     setContent("");
-  }
+  };
+  const handleSubmitForm = async () => {
+    const formData = new FormData();
+    formData.append("content", content);
+    formData.append("file", image.dbImage);
+    setLoading(true);
+    const response = await axios.post(
+      "http://localhost:3001/api/v1/tweet",
+      formData,
+    );
+    if (response) {
+      setLoading(false);
+    }
+    console.log(response);
+    setImage({ preview: "", data: "" });
+    setContent("");
+  };
 
-  return loading ? 
-  <div className="bg-white flex justify-center items-center rounded-md py-8">
-     <Spin/>
-  </div>
-  : (
+  return loading ? (
+    <div className="bg-white flex justify-center items-center rounded-md py-8">
+      <Spin />
+    </div>
+  ) : (
     <div className="py-4 bg-white rounded-md">
       <div className="flex px-8 gap-x-4">
         <div className="rounded-full">
@@ -58,7 +60,7 @@ const CreatePost = () => {
         </div>
         <form className="w-full rounded">
           <textarea
-            onChange={(e)=>handleInputChange(e)}
+            onChange={(e) => handleInputChange(e)}
             value={content}
             rows={2}
             data-autoresize={true}
@@ -68,45 +70,51 @@ const CreatePost = () => {
             id=""
           ></textarea>
         </form>
-
       </div>
       <div className="w-full flex justify-center pl-[5.3rem] pr-[2rem]">
-        {
-          image.preview && 
+        {image.preview && (
           <div className="py-4">
             <img className="rounded-md" src={image.preview} alt="preview" />
           </div>
-        }
+        )}
       </div>
       <div className="flex justify-between px-8">
         <ul className="flex gap-4 py-4">
-        <li className="bg-gray-200 rounded-sm px-[4px] py-[2px] cursor-pointer">
-          <input onChange={(e)=>handleImageChange(e)} id="pc" type="file" className="hidden"/>
-          <label htmlFor="pc" className="flex gap-x-2 text-sm items-center justify-center cursor-pointer">
-            <FaImage color="#15B392" />
-            Photos
-          </label>
-        </li>
-        <li className="bg-gray-200 rounded-sm px-[4px] py-[2px] cursor-pointer">
-          <span className="flex gap-x-2 text-sm items-center justify-center cursor-pointer">
-            <FaVideo color="#00CCDD" />
-            Vedio
-          </span>
-        </li>
+          <li className="bg-gray-200 rounded-sm px-[4px] py-[2px] cursor-pointer">
+            <input
+              onChange={(e) => handleImageChange(e)}
+              id="pc"
+              type="file"
+              className="hidden"
+            />
+            <label
+              htmlFor="pc"
+              className="flex gap-x-2 text-sm items-center justify-center cursor-pointer"
+            >
+              <FaImage color="#15B392" />
+              Photos
+            </label>
+          </li>
+          <li className="bg-gray-200 rounded-sm px-[4px] py-[2px] cursor-pointer">
+            <span className="flex gap-x-2 text-sm items-center justify-center cursor-pointer">
+              <FaVideo color="#00CCDD" />
+              Vedio
+            </span>
+          </li>
 
-        <li className="bg-gray-200 rounded-sm px-[4px] py-[2px] cursor-pointer">
-          <span className="flex gap-x-2 text-sm items-center justify-center cursor-pointer">
-            <BsFillCalendarDateFill color="#D91656" />
-            Event
-          </span>
-        </li>
+          <li className="bg-gray-200 rounded-sm px-[4px] py-[2px] cursor-pointer">
+            <span className="flex gap-x-2 text-sm items-center justify-center cursor-pointer">
+              <BsFillCalendarDateFill color="#D91656" />
+              Event
+            </span>
+          </li>
 
-        <li className="bg-gray-200 rounded-sm px-[4px] py-[2px] cursor-pointer">
-          <span className="flex gap-x-2 text-sm items-center justify-center cursor-pointer">
-            <MdEmojiEmotions color="#FF7F3E" />
-            Feeling/Activity
-          </span>
-        </li>
+          <li className="bg-gray-200 rounded-sm px-[4px] py-[2px] cursor-pointer">
+            <span className="flex gap-x-2 text-sm items-center justify-center cursor-pointer">
+              <MdEmojiEmotions color="#FF7F3E" />
+              Feeling/Activity
+            </span>
+          </li>
         </ul>
         <button onClick={handleSubmitForm}>Post</button>
       </div>
--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,18 +1,35 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 const Trending = () => {
+  const [trending, setTrending] = useState([]);
+  useEffect(() => {
+    getAllTrending();
+  }, []);
+
+  const getAllTrending = async () => {
+    const res = await axios.get("http://localhost:3001/api/v1/trending");
+    setTrending(res.data.data);
+    console.log(res.data.data);
+  };
+
   return (
     <div className="rounded-md bg-white py-4 px-4">
-      <div className="leading-2">
-        <div className="flex flex-col leading-4 cursor-pointer">
-          <span className="text-gray-500 text-[12px] flex flex-col gap-y-1">
-            Trending in INDIA
-          </span>
-          <div className="flex justify-between">
-            <span className="font-semibold">#something</span>
-            <span className="text-gray-500 text-[12px] flex flex-col gap-y-1">
-              23 posts
-            </span>
-          </div>
-        </div>
+      <div className="leading-2 flex flex-col gap-2">
+        {trending.map((trend) => {
+          return (
+            <div key={trend.id} className="flex flex-col px-2 py-2 rounded-lg leading-4 cursor-pointer bg-gray-100 hover:bg-gray-200">
+              <span className="text-gray-500 text-[12px] flex flex-col gap-y-1">
+                Trending in INDIA
+              </span>
+              <div className="flex justify-between">
+                <span className="font-semibold">{trend.hashtag}</span>
+                <span className="text-gray-500 text-[12px] flex flex-col gap-y-1">
+                 {trend.tweets.length} Posts
+                </span>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
