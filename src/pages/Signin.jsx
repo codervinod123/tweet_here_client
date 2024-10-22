f34881eaab1f5977abe6b3659ea3a35e569b1608
Vinod Prajapati <vinodpr737947@gmail.com>
2024-10-22 21:47:59 +0530

    auth:responsiveness added

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -3,11 +3,11 @@ import Quote from "../components/Quote";
 
 const Signin = () => {
   return (
-    <div className="flex w-screen h-screen bg-gray-200 justify-between">
-      <div className="flex justify-center items-center w-screen h-screen">
+    <div className="flex flex-col w-screen bg-gray-200 justify-between lg:flex-row lg:h-screen">
+      <div className="flex justify-center items-center w-screen">
         <Auth comp={"signin"} />
       </div>
-      <div className="flex justify-center items-center w-screen h-screen">
+      <div className="flex justify-center items-center w-screen">
         <Quote />
       </div>
     </div>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,7 +3,7 @@ import Quote from "../components/Quote";
 
 const Signup = () => {
   return (
-    <div className="flex w-screen h-screen bg-gray-200 justify-between">
+    <div className="flex flex-col w-screen bg-gray-200 justify-between lg:flex-row lg:h-screen">
       <div className="flex justify-center items-center w-screen h-screen">
         <Auth comp={"signup"} />
       </div>
