77681d344d844cc9375330e66706b95edd53a5b5
Vinod Prajapati <vinodpr737947@gmail.com>
2024-10-29 00:12:43 +0530

    feat: story ui added

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,31 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BsPlus } from "react-icons/bs";
+import "./global.css";
+import StoryImg1 from "../assets/quote1.jpg";
+import StoryImg2 from "../assets/quote2.jpg";
+import StoryImg3 from "../assets/quote3.jpg";
+
+const stories = [StoryImg1, StoryImg2, StoryImg3, StoryImg1, StoryImg2, StoryImg3, StoryImg1, StoryImg2, StoryImg3];
 
 const Story = () => {
-  return (
-    <div className='grid grid-cols-4 gap-4 h-[150px]'>
-      
-        <div className='h-[150px]'>
-          <label htmlFor="story" className='h-full p-3 cursor-pointer bg-gray-300 rounded flex flex-col justify-center items-center border-2 border-dotted '>
-             <span className='p-2 bg-gray-200 rounded-full'>
-               <BsPlus />
-             </span>
-             <span className='text-sm'>Add story</span>
-          </label>
-          <input id='story' type="text" className='hidden'/>
-        </div>
-        <div className='p-3 bg-gray-300 rounded'>
-           <BsPlus />
-        </div>
-        <div className='p-3 bg-gray-300 rounded'>
-           <BsPlus />
-        </div>
-        <div className='p-3 bg-gray-300 rounded'>
-           <BsPlus />
-        </div>
 
-    </div>
-  )
+   const [story, setStory] = useState({
+      preview: null,
+      data: null
+   });
+   const handleImageUpload = (e) => {
+      setStory({
+         preview: URL.createObjectURL(e.target.files[0]),
+         data: e.target.files[0]
+      });
+      console.log("fileeeeeeeeeeee", e.target.files[0]);
+   }
+
+   const sendStoryRequest = () => {
+      //  const respo=await ();
+      setStory({
+         preview: null,
+         data: null
+      });
+   }
+
+   return (
+      <div className='flex flex-nowrap gap-4 pb-2 w-full'>
+
+         {
+            story.preview ?
+               <div className='flex flex-col gap-1'>
+                  <div className='w-[80px] h-[120px] rounded-sm bg-white flex items-center justify-center'>
+                     <span><img className='rounded-sm' src={story.preview} alt="story1" /></span>
+                  </div>
+                  <button onClick={sendStoryRequest} className='bg-gray-400 px-1 text-gray-800 rounded-md'>Post</button>
+               </div>
+               :
+               <label htmlFor="story">
+                  <div className='w-[80px] cursor-pointer h-[120px] rounded-sm bg-white flex flex-col items-center justify-center'>
+                     <div className='p-2 rounded-full bg-gray-300 '>
+                        <BsPlus />
+                     </div>
+                     <input onChange={handleImageUpload} id="story" type="file" className='hidden' />
+                     <span className='text-xs'>Add story</span>
+                  </div>
+               </label>
+         }
+
+
+         <div className='flex flex-nowrap gap-4 pb-2 w-full overflow-x-auto'>
+            <div class="flex gap-4">
+               {
+                  stories.map((img, index) => {
+                     return (
+                        <div key={index} className='w-[80px] h-[120px] rounded-sm bg-white flex items-center justify-center'>
+                           <span><img className='rounded-sm' src={img} alt="story1" /></span>
+                        </div>
+                     )
+                  })
+               }
+            </div>
+         </div>
+
+      </div>
+   )
 }
 
 export default Story
