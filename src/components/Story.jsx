ce8d5d80ca23df7dfb19a2cb036bf6e75d85886b
Vinod Prajapati <vinodpr737947@gmail.com>
2025-01-11 12:23:56 +0530

    story modal fixed

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -2,12 +2,10 @@ import React, { useEffect, useRef, useState } from 'react'
 import { BsPlus } from "react-icons/bs";
 import "./global.css";
 import axios from 'axios';
-import { Modal, Spin } from "antd";
+import { Spin } from "antd";
 import { LoadingOutlined } from "@ant-design/icons";
 import { toast } from 'react-toastify';
-import { GrNext } from "react-icons/gr";
-import { RxCross2 } from "react-icons/rx";
-import { GrPrevious } from "react-icons/gr";
+import StoryModal from './StoryModal';
 
 const Story = () => {
 
@@ -23,7 +21,6 @@ const Story = () => {
          preview: URL.createObjectURL(e.target.files[0]),
          data: e.target.files[0]
       });
-      console.log("fileeeeeeeeeeee", e.target.files[0]);
    }
 
    const sendStoryRequest = async () => {
@@ -58,11 +55,11 @@ const Story = () => {
 
    const storyRef = useRef();
    const [page, setPage] = useState(null);
+
    const handleStoryView = (index) => {
       setPage(index);
       storyRef.current.showModal();
    }
-
 
    return (
       <div className='flex flex-nowrap gap-4 pb-2 w-full'>
@@ -97,15 +94,15 @@ const Story = () => {
                {
                   stories.map((story, index) => {
                      return (
-                        <div key={index} onClick={()=>handleStoryView(index)} className='w-[80px] h-[120px] rounded-sm bg-white flex items-center justify-center  border-2 border-blue-300 cursor-pointer'>
+                        <div key={index} onClick={() => handleStoryView(index)} className='w-[80px] h-[120px] rounded-sm bg-white flex items-center justify-center  border-2 border-blue-300 cursor-pointer'>
                            <span><img className='rounded-sm' src={story.content} alt="story1" /></span>
                         </div>
                      )
                   })
                }
             </div>
-            <dialog ref={storyRef} className='outline-none rounded-md'>
-               <StoryModal reference={storyRef} stories={stories} page={page} setPage={setPage}/>
+            <dialog ref={storyRef} className='outline-none rounded border-2 border-pink-700'>
+               <StoryModal reference={storyRef} stories={stories} page={page} setPage={setPage} />
             </dialog>
          </div>
       </div>
@@ -113,94 +110,3 @@ const Story = () => {
 }
 
 export default Story
-
-
-
-
-const StoryModal = ({ reference, stories, page, setPage }) => {
-    
-   const handleClose = () => {
-      reference.current.close();
-   };
-
-   const nextStory=()=>{
-      if(page==stories.length-1){
-         reference.current.close();
-         setPage(null);
-         return;
-      }
-      setPage(page=>page+1);
-   };
-
-   const prevStory=()=>{
-      if(page==0){
-         reference.current.close();
-         setPage(null);
-         return;
-      }
-      setPage(page=>page-1);
-   };
-
-   let storyViewTimer;
-   useEffect(()=>{
-      if (reference.current?.open) {
-
-         if(page==stories.length){
-            clearInterval(storyViewTimer);
-            reference.current.close();
-            setPage(null);
-            return;
-         }
-
-         storyViewTimer = setInterval(() => {
-            setPage((page) => page + 1);
-         }, 12000);
-   
-         return () => {
-            clearInterval(storyViewTimer); // Clear interval on cleanup
-         };
-      }
-   },[reference, page]);
-
-   return (
-      <div className='h-[90vh] w-[90vw] overflow-hidden px-4 rounded-md border border-black'>
-        
-         <div className='flex justify-end mt-1'>
-           <span onClick={handleClose} className='p-2 bg-gray-400 rounded-full cursor-pointer hover:bg-gray-300 transition-all duration-500'>
-             <RxCross2 size={"1.5rem"} />
-           </span>
-         </div>
-
-         <div className=' flex justify-between items-center px-4'>
-            <span onClick={prevStory} className='bg-gray-400 p-3 rounded-full text-white font-bold cursor-pointer flex items-center justify-center hover:bg-gray-300 transition-all duration-500'>
-               <GrPrevious />
-            </span>
-            {
-               stories.map((story, index) => {
-                  return (
-                     <React.Fragment key={index}>
-                        {
-                           page == index &&
-                           <div className='flex justify-center flex-col gap-4 rounded-md'>
-                              <div className="h-1 w-full bg-gray-200 rounded-full mt-[-30px] overflow-hidden">
-                                 <div className="h-full bg-blue-500 w-0 animate-fill"></div>
-                              </div>
-                              <div key={index} className='w-full flex justify-center rounded-md'>
-                                 <img className='rounded-md w-[70%]' src={story.content} alt="story" />
-                              </div>
-                           </div>
-                        }
-                     </React.Fragment>
-                  )
-               })
-            }
-            <span onClick={nextStory} className='bg-gray-400 p-3 rounded-full text-white font-bold cursor-pointer flex items-center justify-center hover:bg-gray-300 transition-all duration-500'>
-               <GrNext />
-            </span>
-         </div>
-
-      </div>
-   )
-}
-
-
---
+++ src/components/StoryModal.jsx
@@ -0,0 +1,96 @@
+import React, { useEffect } from 'react'
+import "./global.css";
+import { GrNext } from "react-icons/gr";
+import { RxCross2 } from "react-icons/rx";
+import { GrPrevious } from "react-icons/gr";
+
+const StoryModal = ({ reference, stories, page, setPage }) => {
+    const handleClose = () => {
+       reference.current.close();
+    };
+ 
+    const nextStory = () => {
+       if (page == stories.length - 1) {
+          reference.current.close();
+          setPage(null);
+          return;
+       }
+       setPage(page => page + 1);
+    };
+ 
+    const prevStory = () => {
+       if (page == 0) {
+          reference.current.close();
+          setPage(null);
+          return;
+       }
+       setPage(page => page - 1);
+    };
+ 
+    let storyViewTimer;
+ 
+    useEffect(() => {
+       if (reference.current?.open) {
+          if (page == stories.length) {
+             clearInterval(storyViewTimer);
+             reference.current.close();
+             setPage(null);
+             return;
+          }
+ 
+          storyViewTimer = setInterval(() => {
+             setPage((page) => page + 1);
+          }, 12000);
+ 
+          return () => {
+             clearInterval(storyViewTimer); // Clear interval on cleanup
+          };
+       }
+    }, [reference, page]);
+ 
+    return (
+       <div onClick={handleClose} className='fixed inset-0 flex justify-center items-center transition-colors m-8 bg-gray-200/90'>
+          <div className='absolute top-2 right-2'>
+             <button onClick={handleClose} className='p-1 rounded-full outline-none bg-gray-400/50 hover:bg-gray-300 transition-all duration-500'>
+                <RxCross2 size={"1.5rem"} color='red' />
+             </button>
+          </div>
+          <div onClick={(e) => e.stopPropagation()} className='mx-8 my-8 border'>
+             <div className='flex gap-4 justify-between items-center'>
+                <button onClick={prevStory} className='bg-gray-400 p-2 rounded-full text-white font-bold cursor-pointer flex items-center justify-center hover:bg-gray-300 transition-all duration-500'>
+                   <GrPrevious />
+                </button>
+                <div>
+                   {
+                      stories.map((story, index) => {
+                         return (
+                            <React.Fragment key={index}>
+                               {
+                                  page == index &&
+                                  <div className='flex justify-center flex-col gap-4 rounded-md'>
+                                     <div className="h-1 w-full bg-gray-200 rounded-full mt-[-30px] overflow-hidden">
+                                        <div className="h-full bg-blue-500 w-0 animate-fill"></div>
+                                     </div>
+                                     <div>
+                                       <img className='rounded w-96 max-h-[75vh]' src={story.content} alt={story.content} />
+                                     </div>
+                                  </div>
+                               }
+                            </React.Fragment>
+                         )
+                      })
+                   }
+                </div>
+                <button onClick={nextStory} className='bg-gray-400 p-2 rounded-full text-white font-bold cursor-pointer flex items-center justify-center hover:bg-gray-300 transition-all duration-500'>
+                   <GrNext />
+                </button>
+             </div>
+          </div>
+       </div>
+    )
+ }
+ 
+
+ export default StoryModal;
+ 
+ 
