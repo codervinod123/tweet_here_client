39b83f85d17d340f551bd33147fe8a53cf079672
Vinod Prajapati <vinodpr737947@gmail.com>
2025-02-14 10:10:04 +0530

    fixed DB issue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
   
   
     const loginuser = async() =>{
-      const user  = await axios.get("http://localhost:3000/api/v1/user/currentloginuser", {headers:{ token: localStorage.getItem("token") }});
+      const user  = await axios.get("https://tweet-here.onrender.com/api/v1/user/currentloginuser", {headers:{ token: localStorage.getItem("token") }});
       setLoginUser(user.data.data);
     }
 
